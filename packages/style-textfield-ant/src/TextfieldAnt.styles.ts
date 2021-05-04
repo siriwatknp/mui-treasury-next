febed5d0443dd09c3ac10f0ecb3815c0400d22aa
siriwatknp <siriwatkunaporn@gmail.com>
2021-05-04 17:38:15 +0700

    feat(style-textfield-ant): add antd input style

---
+++ packages/style-textfield-ant/src/TextfieldAnt.styles.ts
@@ -0,0 +1,191 @@
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Components } from "@material-ui/core/styles/components";
+
+type Output = Required<
+  Pick<
+    Components,
+    | "MuiInputLabel"
+    | "MuiInput"
+    | "MuiInputAdornment"
+    | "MuiSelect"
+    | "MuiFormHelperText"
+  >
+>;
+
+const getPlaceholderVisible = (theme: Theme) => {
+  const light = theme.palette.mode === "light";
+  return {
+    opacity: `${light ? 0.32 : 0.5} !important`,
+    fontWeight: 500,
+    letterSpacing: 0,
+  };
+};
+export const createTextfieldAntStyles = (theme: Theme): Output => {
+  const placeholderVisible = getPlaceholderVisible(theme);
+  return {
+    MuiInputLabel: {
+      styleOverrides: {
+        // @ts-ignore
+        standard: {
+          fontSize: 14,
+          transform: "none",
+          color: theme.palette.text.primary,
+          "&.Mui-focused": {
+            color: theme.palette.text.primary,
+          },
+          "&.Mui-error": {
+            color: theme.palette.text.primary,
+          },
+        },
+      },
+    },
+    MuiInput: {
+      defaultProps: {
+        disableUnderline: true,
+      },
+      styleOverrides: {
+        root: {
+          border: "1px solid",
+          borderColor: "#d9d9d9",
+          borderRadius: 2,
+          backgroundColor: theme.palette.background.paper,
+          fontSize: 14,
+          lineHeight: 1.5715,
+          "label + &": {
+            marginTop: 24,
+          },
+          "&:hover": {
+            borderColor: "#40a9ff",
+          },
+          "&.Mui-focused": {
+            borderColor: "#40a9ff",
+            boxShadow: "0 0 0 2px rgb(24 144 255 / 20%)",
+          },
+          "&.Mui-error": {
+            borderColor: "#ff4d4f",
+            "&.Mui-focused": {
+              boxShadow: "0 0 0 2px rgb(255 77 79 / 20%)",
+            },
+          },
+          "&&& .MuiInput-input": {
+            // tripple '&' to override default behaviour
+            "&::-webkit-input-placeholder": placeholderVisible,
+            "&::-moz-placeholder": placeholderVisible, // Firefox 19+
+            "&:-ms-input-placeholder": placeholderVisible, // IE11
+            "&::-ms-input-placeholder": placeholderVisible, // Edge
+          },
+          "&.Mui-disabled": {
+            backgroundColor: "#f5f5f5",
+          },
+        },
+        input: {
+          padding: "4px 11px",
+          height: "auto",
+          "&.MuiInputBase-inputAdornedStart": {
+            paddingLeft: 36,
+            "&.MuiInputBase-inputSizeSmall": {
+              paddingLeft: 36,
+            },
+          },
+          "&.MuiInputBase-inputAdornedEnd": {
+            paddingRight: 36,
+            "&.MuiInputBase-inputSizeSmall": {
+              paddingRight: 36,
+            },
+          },
+        },
+      },
+    },
+    MuiInputAdornment: {
+      styleOverrides: {
+        root: {
+          "&.MuiInputAdornment-standard": {
+            position: "absolute",
+            color: theme.palette.text.primary,
+            "& svg": {
+              fontSize: "1.25rem",
+            },
+          },
+        },
+        positionStart: {
+          left: 8,
+        },
+        positionEnd: {
+          right: 8,
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          "&.MuiSelect-standard": {
+            paddingRight: 32,
+          },
+        },
+        // @ts-ignore
+        standard: {
+          "&.MuiInputBase-inputAdornedEnd": {
+            paddingRight: 60,
+            "&.MuiInputBase-inputSizeSmall": {
+              paddingRight: 60,
+            },
+          },
+        },
+        // @ts-ignore
+        iconStandard: {
+          right: 6,
+          "& + .MuiInputAdornment-standard": {
+            right: 32,
+          },
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          marginTop: 0,
+          lineHeight: 1.5715,
+          fontSize: 14,
+          "&.Mui-error": {
+            color: "#ff4d4f",
+          },
+        },
+      },
+    },
+  };
+};
+
+const useInputLabelStyles = makeStyles(
+  // @ts-expect-error
+  (theme) => createTextfieldAntStyles(theme).MuiInputLabel.styleOverrides
+);
+
+const useInputStyles = makeStyles(
+  // @ts-expect-error
+  (theme) => createTextfieldAntStyles(theme).MuiInput.styleOverrides
+);
+
+const useInputAdornmentStyles = makeStyles(
+  // @ts-expect-error
+  (theme) => createTextfieldAntStyles(theme).MuiInputAdornment.styleOverrides
+);
+
+const useSelectStyles = makeStyles(
+  // @ts-expect-error
+  (theme) => createTextfieldAntStyles(theme).MuiSelect.styleOverrides
+);
+
+const useFormHelperTextStyles = makeStyles(
+  // @ts-expect-error
+  (theme) => createTextfieldAntStyles(theme).MuiFormHelperText.styleOverrides
+);
+
+export const useTextfieldAntStyles = () => {
+  return {
+    InputLabel: useInputLabelStyles(),
+    Input: useInputStyles(),
+    InputAdornment: useInputAdornmentStyles(),
+    Select: useSelectStyles(),
+    FormHelperText: useFormHelperTextStyles(),
+  };
+};
