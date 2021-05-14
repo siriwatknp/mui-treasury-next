05f50302ef51d212af7bcc632d8768b1aad334b1
siriwatknp <siriwatkunaporn@gmail.com>
2021-05-14 08:44:46 +0700

    chore: refactor to all varians

--- stories/material-ui/MuiSelect.stories.tsx
+++ stories/material-ui/MuiSelect.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputAdornment from "@material-ui/core/InputAdornment";
@@ -11,196 +12,187 @@ export default {
   title: "Material-UI/Select",
 } as Meta;
 
-export const SelectField: Story = () => {
+export const AllVariants = () => {
   const [value, setValue] = useState("");
   return (
-    <>
-      <TextField
-        label="Label"
-        size="small"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        helperText="This is a helper text"
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-      <br />
-      <br />
-      <TextField
-        label="Label"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        helperText="This is a helper text"
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-    </>
-  );
-};
+    <Grid container spacing={2}>
+      <Grid item>
+        <TextField
+          label="Label 0"
+          size="small"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          helperText="This is a helper text"
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+        <br />
+        <br />
+        <TextField
+          label="Label 1"
+          size="small"
+          error
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          helperText="This is a helper text"
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+      </Grid>
 
-export const SelectError: Story = () => {
-  const [value, setValue] = useState("");
-  return (
-    <>
-      <TextField
-        label="Label"
-        size="small"
-        error
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        helperText="This is a helper text"
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-      <br />
-      <br />
-      <TextField
-        label="Label"
-        error
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        helperText="This is a helper text"
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-    </>
-  );
-};
-
-export const SelectIcon: Story = () => {
-  const [value, setValue] = useState("");
-  return (
-    <>
-      <TextField
-        label="Label"
-        size="small"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MailOutlined />
-            </InputAdornment>
-          ),
-        }}
-        margin="dense"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-      <TextField
-        label="Label"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <MailOutlined />
-            </InputAdornment>
-          ),
-        }}
-        margin="dense"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        sx={{ ml: 2 }}
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-      <br />
-      <br />
-      <TextField
-        label="Label"
-        size="small"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <MailOutlined />
-            </InputAdornment>
-          ),
-        }}
-        margin="dense"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-      <TextField
-        label="Label"
-        select
-        SelectProps={{ displayEmpty: true }}
-        InputLabelProps={{ shrink: true }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <MailOutlined />
-            </InputAdornment>
-          ),
-        }}
-        margin="dense"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        sx={{ ml: 2 }}
-      >
-        <MenuItem value="">
-          <em>Select Option</em>
-        </MenuItem>
-        <MenuItem value={1}>Option 1</MenuItem>
-        <MenuItem value={2}>Option 2</MenuItem>
-        <MenuItem value={3}>Option 3</MenuItem>
-      </TextField>
-    </>
+      <Grid item>
+        <TextField
+          label="Label 2"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          helperText="This is a helper text"
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+        <br />
+        <br />
+        <TextField
+          label="Label 3"
+          error
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          helperText="This is a helper text"
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+      </Grid>
+      <Grid item>
+        <TextField
+          label="Label 4"
+          size="small"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <MailOutlined />
+              </InputAdornment>
+            ),
+          }}
+          margin="dense"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+        <TextField
+          label="Label 5"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <MailOutlined />
+              </InputAdornment>
+            ),
+          }}
+          margin="dense"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          sx={{ ml: 2 }}
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+        <br />
+        <br />
+        <TextField
+          label="Label 6"
+          size="small"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <MailOutlined />
+              </InputAdornment>
+            ),
+          }}
+          margin="dense"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+        <TextField
+          label="Label 7"
+          select
+          SelectProps={{ displayEmpty: true }}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <MailOutlined />
+              </InputAdornment>
+            ),
+          }}
+          margin="dense"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          sx={{ ml: 2 }}
+        >
+          <MenuItem value="">
+            <em>Select Option</em>
+          </MenuItem>
+          <MenuItem value={1}>Option 1</MenuItem>
+          <MenuItem value={2}>Option 2</MenuItem>
+          <MenuItem value={3}>Option 3</MenuItem>
+        </TextField>
+      </Grid>
+    </Grid>
   );
 };
